dc3f5fa8eef8b9a70150853b75ff1b1edfccf547
rinrinrin4 <moihor.12.03@gmail.com>
2026-02-25 16:19:37 +0300

    v3

--- 3 zadanie.py
+++ 3 zadanie.py
@@ -1,28 +1,28 @@
-pi=3.141592
-r=int(input("Введите радиус круга "))
-S=pi*r**2
-print(S)
+# pi=3.141592
+# r=int(input("Введите радиус круга "))
+# S=pi*r**2
+# print(S)
 
 
-a=int(input("Введите значение a "))
-b=int(input("Введите значение b "))
-x=int(-b/a)
-print(x)
+# a=int(input("Введите значение a "))
+# x=int(input("Введите значение b "))
+# x=int(-b/a)
+# print(x)
+#
+#
+# C=int(input("Введите температуру "))
+# F=int(9/5*C+32)
+# print(F)
+#
+#
+# W=int(input("Введите значение "))
+# D=int(input("Введите значение "))
+# R=int(input("Введите значение "))
+# AR=int((W+D+R)/3)
+# print(AR)
 
-
-C=int(input("Введите температуру "))
-F=int(9/5*C+32)
-print(F)
-
-
-W=int(input("Введите значение "))
-D=int(input("Введите значение "))
-R=int(input("Введите значение "))
-AR=int((W+D+R)/3)
-print(AR)
-
-N1=5 + 2 * 3 - 4 / 2
-N2=(3 + 5) * (2 + 4) / 2
-N3=-3 + 6 / 2 * 4
-N4=5+4*5**2+7
-print(N1,N2,N3,N4)
+N1= int(5 + 2 * 3 - 4 / 2)
+N2=int((3 + 5) * (2 + 4) / 2)
+N3=int(-3 + 6 / 2 * 4)
+N4=int(5+4*5**2+7)
+print(N1, N2, N3, N4, sep="\n")
